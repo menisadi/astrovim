8d905ec0a712c1afe730e1ca519894c79892289f
sadigu <menis@intentiq.com>
2023-07-24 14:16:50 +0300

    adding some plugins

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,6 +4,8 @@ return {
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
+  { import = "astrocommunity.markdown-and-latex.vimtex" },
+  { import = "astrocommunity.completion.copilot-lua-cmp"},
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
